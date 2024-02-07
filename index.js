9c87726b605e9f9a2d57bf09a946608689aedb05
mingman <ingman.m@gmail.com>
2024-02-07 11:25:38 +0100

    exercise 3

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ xhr.open("get", "https://majazocom.github.io/Data/pokemons.json", true);
 xhr.addEventListener("readystatechange", () => {
   if (xhr.readyState === 4) {
     let pokemons = JSON.parse(xhr.responseText);
-    let list = pokeList(pokemons);
-    console.log(list);
+    allNamesToConsole(pokemons);
+    let list = namesToList(pokemons);
     document.querySelector(".pokemons").appendChild(list);
   } else {
     console.error("Error: ", xhr.status);
@@ -12,15 +12,61 @@ xhr.addEventListener("readystatechange", () => {
 });
 xhr.send();
 
-function pokeList(pokemons) {
+const dogRequest = new XMLHttpRequest();
+dogRequest.open("get", "https://majazocom.github.io/Data/dogs.json", true);
+dogRequest.addEventListener("readystatechange", () => {
+  if (dogRequest.readyState === 4) {
+    let dogs = JSON.parse(dogRequest.responseText);
+    allNamesToConsole(dogs);
+    let dogHTML = namesToList(dogs);
+    console.log(dogHTML);
+    document.querySelector(".dogs").appendChild(dogHTML);
+  } else {
+    console.error("Error: ", dogRequest.status);
+  }
+});
+dogRequest.send();
+
+const bookRequest = new XMLHttpRequest();
+bookRequest.open("get", "https://majazocom.github.io/Data/books.json", true);
+bookRequest.addEventListener("readystatechange", () => {
+  if (bookRequest.readyState === 4) {
+    let books = JSON.parse(bookRequest.responseText);
+    console.log(books);
+    let longBooks = [];
+    books.forEach((book) => {
+      if (book.pages > 500) {
+        longBooks.push(book.title);
+      }
+    });
+    let booksHtml = arrayToList(longBooks);
+    document.querySelector(".books").appendChild(booksHtml);
+  } else {
+    console.error("Error: ", bookRequest.status);
+  }
+});
+bookRequest.send();
+
+function namesToList(listArray) {
   let htmlList = document.createElement("ul");
-  pokemons.forEach((element) => {
+  listArray.forEach((element) => {
     let li = document.createElement("li");
     li.innerText = element.name;
     htmlList.appendChild(li);
   });
   return htmlList;
 }
-function singlePkmn(pokemon) {
-  console.log(pokemon);
+function arrayToList(listArray) {
+  let htmlList = document.createElement("ul");
+  listArray.forEach((element) => {
+    let li = document.createElement("li");
+    li.innerText = element;
+    htmlList.appendChild(li);
+  });
+  return htmlList;
+}
+function allNamesToConsole(list) {
+  list.forEach((item) => {
+    console.log(item.name);
+  });
 }
